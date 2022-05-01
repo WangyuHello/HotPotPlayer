88a26cf4a6b06d61b36569145498cc9347493b16
王渝 <wwangyuu@outlook.com>
2022-05-01 11:46:29 +0800

    更新

--- HotPotPlayer.Interop/MpvRender.cpp
+++ HotPotPlayer.Interop/MpvRender.cpp
@@ -1,2 +1,12 @@
 #include "pch.h"
 #include "MpvRender.h"
+#include "MpvRender.g.cpp"
+
+namespace winrt::HotPotPlayer_Interop::implementation
+{
+    int32_t MpvRender::Render(winrt::Microsoft::Graphics::Canvas::CanvasDrawingSession const& ds)
+    {
+        
+        return 0;
+    }
+}
--- HotPotPlayer.Interop/MpvRender.h
+++ HotPotPlayer.Interop/MpvRender.h
@@ -1,5 +1,18 @@
 #pragma once
-class MpvRender
-{
-};
+#include "MpvRender.g.h"
 
+namespace winrt::HotPotPlayer_Interop::implementation
+{
+    struct MpvRender : MpvRenderT<MpvRender>
+    {
+        MpvRender() = default;
+
+        int32_t Render(winrt::Microsoft::Graphics::Canvas::CanvasDrawingSession const& ds);
+    };
+}
+namespace winrt::HotPotPlayer_Interop::factory_implementation
+{
+    struct MpvRender : MpvRenderT<MpvRender, implementation::MpvRender>
+    {
+    };
+}
