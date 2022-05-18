54a31516777107d3ab571d273e7a86c117cfd78c
王渝 <wwangyuu@outlook.com>
2022-05-18 21:45:40 +0800

    更新

---
+++ HotPotPlayer/Animations/OutOfOrderEntranceThemeAnimation.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.UI.Xaml.Media.Animation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinRT;
+
+namespace HotPotPlayer.Animations
+{
+    public class OutOfOrderEntranceThemeAnimation : Transition
+    {
+        protected OutOfOrderEntranceThemeAnimation(DerivedComposed _) : base(_)
+        {
+        }
+
+        protected internal OutOfOrderEntranceThemeAnimation(IObjectReference objRef) : base(objRef)
+        {
+        }
+    }
+}
