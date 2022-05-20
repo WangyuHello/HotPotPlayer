b31cb828d4e097510ba82e69d2cfb039a306fb98
王渝 <wwangyuu@outlook.com>
2022-05-20 12:29:28 +0800

    更新

--- HotPotPlayer/Pages/CloudMusicSub/Self.xaml.cs
+++ HotPotPlayer/Pages/CloudMusicSub/Self.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using HotPotPlayer.Models;
+using HotPotPlayer.Models.CloudMusic;
 using HotPotPlayer.Services;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
@@ -6,11 +7,14 @@ using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -23,12 +27,24 @@ namespace HotPotPlayer.Pages.CloudMusicSub
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Self : Page
+    public sealed partial class Self : Page, INotifyPropertyChanged
     {
         public Self()
         {
             this.InitializeComponent();
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Set<T>(ref T oldValue, T newValue, [CallerMemberName] string propertyName = "")
+        {
+            if (!EqualityComparer<T>.Default.Equals(oldValue, newValue))
+            {
+                oldValue = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
         NetEaseMusicService CloudMusicService => ((App)Application.Current).NetEaseMusicService;
         MusicPlayer Player => ((App)Application.Current).MusicPlayer;
 
@@ -47,16 +63,32 @@ namespace HotPotPlayer.Pages.CloudMusicSub
             IsFirstNavigate = false;
         }
 
+        private CloudPlayListItem _selectedPlayList;
+        public CloudPlayListItem SelectedPlayList
+        {
+            get => _selectedPlayList;
+            set => Set(ref _selectedPlayList, value);
+        }
+
         private void LikeList_ItemClick(object sender, ItemClickEventArgs e)
         {
             var m = e.ClickedItem as MusicItem;
             Player.PlayNext(m, CloudMusicService.LikeList);
         }
 
-        private void PlayLists_ItemClick(object sender, ItemClickEventArgs e)
+        private async void PlayLists_ItemClick(object sender, ItemClickEventArgs e)
         {
-            var m = e.ClickedItem as PlayListItem;
-            
+            var m = e.ClickedItem as CloudPlayListItem;
+            SelectedPlayList = await CloudMusicService.GetPlayListAsync(m.PlId);
+            PlayListPopupOverlay.Visibility = Visibility.Visible;
+        }
+
+        private void PlayListPopupOverlay_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            //var anim = ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("backwardsAnimation2", PlayListPopupTarget);
+            //anim.Configuration = new BasicConnectedAnimationConfiguration();
+            //await PlayListGridView.TryStartConnectedAnimationAsync(anim, SelectedPlayList, "PlayListCardConnectedElement");
+            PlayListPopupOverlay.Visibility = Visibility.Collapsed;
         }
     }
 }
