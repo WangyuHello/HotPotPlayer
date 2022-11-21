2549f31cc6dd1af3539bfb4b376dfb8d6bc2d889
王渝 <wwangyuu@outlook.com>
2022-11-21 22:50:02 +0800

    更新

--- HotPotPlayer.Video/VideoControl.xaml.cs
+++ HotPotPlayer.Video/VideoControl.xaml.cs
@@ -57,53 +57,69 @@ namespace HotPotPlayer.Video
         {
             get
             {
-                _mpv ??= new MpvPlayer(@"NativeLibs\mpv-2.dll")
+                if (_mpv == null)
+                {
+                    _mpv = new MpvPlayer(IntPtr.Zero+1, @"NativeLibs\mpv-2.dll")
                     {
                         AutoPlay = true,
                         Volume = 100,
                         LogLevel = MpvLogLevel.Debug
                     };
-                _mpv.SetGpuNextD3DInitCallback(GpuNextD3DInitCallback);
+                    _mpv.SetGpuNextD3DInitCallback(GpuNextD3DInitCallback);
+                }
+
                 return _mpv;
             }
         }
 
         IDXGISwapChain1 _swapChain1;
-        DirectN.ID3D11Device1 _device;
+        IDXGISwapChain2 _swapChain2;
+        DirectN.ID3D11Device _device;
 
         private void GpuNextD3DInitCallback(IntPtr d3d11Device, IntPtr swapChain)
         {
-            //_swapChain1 = (IDXGISwapChain1)Marshal.GetObjectForIUnknown(swapChain);
-            _swapChain1 = ObjectReference<IDXGISwapChain1>.FromAbi(swapChain).Vftbl;
+            _swapChain1 = (IDXGISwapChain1)Marshal.GetObjectForIUnknown(swapChain);
+            _swapChain2 = (IDXGISwapChain2)Marshal.GetObjectForIUnknown(swapChain);
+            _device = (DirectN.ID3D11Device)Marshal.GetObjectForIUnknown(d3d11Device);
+            //_swapChain1 = ObjectReference<IDXGISwapChain1>.FromAbi(swapChain).Vftbl;
+            var nativepanel = Host.As<ISwapChainPanelNative>();
+            _swapChain1.GetDesc1(out var desp);
+            BufferCount = desp.BufferCount;
+            nativepanel.SetSwapChain(_swapChain1);
+            UpdateSize();
+            UpdateScale();
         }
 
         private void StartPlay(FileInfo file)
         {
+            //Mpv.API.SetPropertyString("vo", "gpu");
             Mpv.API.SetPropertyString("vo", "gpu-next");
             Mpv.API.SetPropertyString("gpu-context", "d3d11");
             Mpv.API.SetPropertyString("hwdec", "d3d11va");
+            Mpv.API.SetPropertyString("d3d11-composition", "yes");
 #if DEBUG
             Mpv.API.Command("script-binding", "stats/display-stats-toggle");
 #endif
             Mpv.LoadAsync(file.FullName);
-
-            var nativepanel = Host.As<ISwapChainPanelNative>();
-            nativepanel.SetSwapChain(_swapChain1);
         }
 
         private void Host_Loaded(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void Host_CompositionScaleChanged(SwapChainPanel sender, object args)
         {
-
+            CurrentCompositionScaleX = Host.CompositionScaleX;
+            CurrentCompositionScaleY = Host.CompositionScaleY;
+            UpdateScale();
         }
 
         private void Host_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-
+            CurrentWidth = (uint)Host.ActualWidth;
+            CurrentHeight = (uint)Host.ActualHeight;
+            UpdateSize();
         }
 
         private void Host_Unloaded(object sender, RoutedEventArgs e)
@@ -111,9 +127,33 @@ namespace HotPotPlayer.Video
 
         }
 
+        object _CriticalLock = new object();
+        uint CurrentWidth;
+        uint CurrentHeight;
+        float CurrentCompositionScaleX;
+        float CurrentCompositionScaleY;
+        uint BufferCount;
+
+        void UpdateSize()
+        {
+            if (Host is null || _swapChain1 is null)
+                return;
+
+            lock (_CriticalLock)
+            {
+                _swapChain1?.ResizeBuffers(BufferCount, CurrentWidth, CurrentHeight, DXGI_FORMAT.DXGI_FORMAT_B8G8R8A8_UNORM, 0);
+            }
+        }
+
+        void UpdateScale()
+        {
+            if (Host is null || _swapChain2 is null) return;
+            //_swapChain2.SetMatrixTransform(new DXGI_MATRIX_3X2_F { _11 = 1.0f / CurrentCompositionScaleX, _22 = 1.0f / CurrentCompositionScaleY });
+        }
+
         public void Stop()
         {
-
+            Mpv.Stop();
         }
     }
 
