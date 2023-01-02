7d70cba57bd5a4aeff24605c1e660ca4641841cd
王渝 <wwangyuu@outlook.com>
2023-01-02 18:39:43 +0800

    更新

--- HotPotPlayer/Controls/BilibiliSub/ShareFlyout.xaml.cs
+++ HotPotPlayer/Controls/BilibiliSub/ShareFlyout.xaml.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
+using HotPotPlayer.Services;
 using HotPotPlayer.Services.BiliBili.Video;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
@@ -8,14 +9,18 @@ using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
 using Microsoft.UI.Xaml.Navigation;
+using QRCoder;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
 using Windows.System;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -38,11 +43,36 @@ namespace HotPotPlayer.Controls.BilibiliSub
         }
 
         public static readonly DependencyProperty VideoProperty =
-            DependencyProperty.Register("Video", typeof(VideoContent), typeof(ShareFlyout), new PropertyMetadata(default));
+            DependencyProperty.Register("Video", typeof(VideoContent), typeof(ShareFlyout), new PropertyMetadata(default, VideoChanged));
+
+        private static void VideoChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((ShareFlyout)d).SetShareQrImage(e.NewValue as VideoContent);
+        }
 
         private async void OpenWebClick(object sender, RoutedEventArgs e)
         {
             await Launcher.LaunchUriAsync(new Uri("https://www.bilibili.com/video/" + Video.Bvid));
+        }
+
+        byte[] GetQrImgByte(string url)
+        {
+            var qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            BitmapByteQRCode qrCode = new(qrCodeData);
+            byte[] qrCodeAsBitmapByteArr = qrCode.GetGraphic(20);
+            return qrCodeAsBitmapByteArr;
+        }
+
+        async void SetShareQrImage(VideoContent video)
+        {
+            var qrData = GetQrImgByte("https://m.bilibili.com/video/"+video.Bvid);
+            BitmapImage image = new();
+            var stream = new InMemoryRandomAccessStream();
+            await stream.WriteAsync(qrData.AsBuffer());
+            stream.Seek(0);
+            await image.SetSourceAsync(stream);
+            QR.Source = image;
         }
     }
 }
